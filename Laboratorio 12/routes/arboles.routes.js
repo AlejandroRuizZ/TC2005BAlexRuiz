b7b33dbc1809a3d98593cf1632757ad0378ee453
AlejandroRuizZ <a01611451@itesm.mx>
2022-03-14 23:30:02 -0600

    Lab 12 terminado

--- Laboratorio 12/routes/arboles.routes.js
+++ Laboratorio 12/routes/arboles.routes.js
@@ -1,7 +1,10 @@
 const express = require('express');
 
 const router = express.Router();
-const arboles = ["Olmo", "Pino", "Roble"];
+const arboles = [{nombre:"Olmo"}, 
+{nombre:"Pino"}, 
+{nombre:"Roble"}
+];
 const filesystem = require('fs');
 const path = require('path');
 
@@ -10,23 +13,17 @@ router.get('/Alex', (request, response, next) => {
 });
 
 router.get('/',(request, response, next) =>{
-    let respuesta = '<!DOCTYPE html><html lang="es-mx"><head><title>Arboles</title><meta charset="utf-8"></meta></head><body><h1>Arboles</h1><main><h2>Existen Arboles</h2><p>Estos son los Arboles:</p><ol>';
-    for (let i in arboles){
-        respuesta += '<li>' + arboles[i] + '</li>';
-    }
-    respuesta += '</ol><a href="/arboles/nuevo">Agregar un nuevo Arbol</a></main></body></html>'
+    response.render('lista', {arboles: arboles });
     response.send(respuesta);
 });
 
 router.get('/nuevo',(request, response, next) =>{
-    console.log(request.body);
-    let respuesta = '<!DOCTYPE html><html lang="es-mx"><head><title>Arboles</title><meta charset="utf-8"></meta></head><body><h1>Bienvenido a este sitio de Arboles</h1><main><h2>Aquí se habla se de nuevos Arboles</h2><p>Ingresa los datos del nuevo Arbol:</p><form action="/arboles/nuevo" method="POST"><label for="nombre">Nombre del nuevo Arbol: </label><input type="text" name="nombre" required><input type="submit" value="Enviar"></form><br><br><a href="/arboles">Regresar a la lista de Arboles</a></br></body></html>';
-    response.send(respuesta);
+    response.render('nuevo',{nombre: 'Alex'})
 });
 
 router.post('/nuevo',(request,response,next) =>{
     console.log(request.body);
-    arboles.push(request.body.nombre);
+    arboles.push({nombre: request.body.nombre});
     console.log(arboles);
     filesystem.writeFileSync('Arboles.txt', request.body.nombre)
     response.status(303);
